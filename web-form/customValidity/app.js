36c8fbe3d1ef97d80126459d8a700bbfa9480511
yukicode <yolandaapply@gmail.com>
2016-11-16 14:30:38 -0800

    Modify validation
    Add IssueTracker class
    Add HashSet class
    Separate validation check from submit action

--- web-form/customValidity/app.js
+++ web-form/customValidity/app.js
@@ -25,23 +25,114 @@ var patternIllegalChar = /[^A-z0-9\!\@\#\$\%\^\&\*]/g;
 You'll probably find this function useful...
  */  
 submit.onclick = function () {
-    var pw = firstPasswordInput.value;
-    if(pw !== secondPasswordInput.value){
-        firstPasswordInput.setCustomValidity("The passwords are not the same");
-    }else if(pw.length <1){
-        firstPasswordInput.setCustomValidity("Password is too short!");
-    }else if(pw.length>100) {
-        firstPasswordInput.setCustomValidity("Password is too long!");
-    }else if(pw.search(patternNum)<0){
-        firstPasswordInput.setCustomValidity("Password must contain a number!");
-    }else if(pw.search(patternLowercase)<0){
-        firstPasswordInput.setCustomValidity("Password must contain a lowercase letter!");
-    }else if(pw.search(patternUppercase)<0){
-        firstPasswordInput.setCustomValidity("Password must contain a UPPERCASE letter!");
-    }else if(pw.search(patternSpecialChar)>0){
-        firstPasswordInput.setCustomValidity("Password can't contain special chars @, !, #, $, %, ^, &, *");
-    }else{
-        firstPasswordInput.setCustomValidity("");
+    var firstPW = firstPasswordInput.value;
+    var secondPW = secondPasswordInput.value;
+    var firstPWTracker = new IssueTracker();
+    var secondPWTracker = new IssueTracker();
+
+    checkValidity(firstPW, secondPW, firstPWTracker, secondPWTracker);
+    firstPasswordInput.setCustomValidity(firstPWTracker.getIssues());
+    secondPasswordInput.setCustomValidity(secondPWTracker.getIssues());
+};
+
+function checkValidity(firstPW, secondPW, firstPWTracker, secondPWTracker){
+    if(firstPW !== secondPW){
+        secondPWTracker.add(Issue.NOT_EQUAL);
+        return;
     }
 
+    if(firstPW.length < 16){
+        firstPWTracker.add(Issue.PW_SHORT);
+    }
+
+    if(firstPW.length > 100){
+        firstPWTracker.add(Issue.PW_LONG);
+    }
+
+    if(firstPW.search(patternNum) < 0){
+        firstPWTracker.add(Issue.NEED_NUM);
+    }
+
+    if(firstPW.search(patternUppercase) < 0){
+        firstPWTracker.add(Issue.NEED_UPPERCASE);
+    }
+
+    if(firstPW.search(patternLowercase) < 0){
+        firstPWTracker.add(Issue.NEED_LOWERCASE);
+    }
+
+    if(firstPW.search(patternSpecialChar) < 0){
+        firstPWTracker.add(Issue.NEED_SPECIAL);
+    }
+    
+    var illegalChars = firstPW.match(patternIllegalChar);
+    if(illegalChars){
+        var allUniqueChars = new HashSet();
+        illegalChars.forEach(function(c){
+            allUniqueChars.add(c);
+        });
+        for(var property in allUniqueChars.getAll()){
+            if(allUniqueChars.contains(property)){
+                firstPWTracker.add(Issue.ILLEGAL_CHAR(property));
+            }
+        }
+    }
+}
+
+var Issue = {
+    PW_SHORT: "The password should have at least 16 charactors.",
+    PW_LONG: "The password should have less than 100 charactors",
+    NOT_EQUAL: "The second password is different from the first one",
+    NEED_NUM : "Need at least a number.",
+    NEED_LOWERCASE: "Need at least a lowercase letter",
+    NEED_UPPERCASE: "Need at least a uppercase letter",
+    NEED_SPECIAL: "Need at least a special charactor: !, @, #, $, %, ^, &, *.",
+    ILLEGAL_CHAR: function(c){
+        return "Remove illegal charactor: " + c;
+    }
+};
+
+function IssueTracker(){
+    this.issues = [];
+}
+
+IssueTracker.prototype.add = function(s){
+    if(s){
+        this.issues.push(s);
+    }
+};
+
+IssueTracker.prototype.getIssues = function(){
+    if(this.issues.length === 0){
+        return "";
+    }
+    if(this.issues.length === 1){
+        return "Please fix the following problem: " + this.issues[0];
+    }
+    if(this.issues.length >1){
+        var problems = "";
+        this.issues.forEach(function(issue){
+            problems += issue;
+            problems += "\n";
+        });
+        return "Please fix the following problems: " + problems;
+    }
+};
+
+function HashSet(){
+    this.set = {};
+}
+
+HashSet.prototype.add = function(key){
+    this.set[key] = true;
+};
+
+HashSet.prototype.contains = function(key){
+    if(this.set.hasOwnProperty(key)){
+        return true;
+    }
+};
+
+HashSet.prototype.getAll = function(){
+    return this.set;
 };
