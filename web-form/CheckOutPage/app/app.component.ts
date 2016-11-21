2985f009a3faebbb4571a063b3a9c11f4a25359c
yukicode <yolandaapply@gmail.com>
2016-11-20 21:24:34 -0800

    set up angular 2

---
+++ web-form/CheckOutPage/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h2>My Checkout</h2>
+    `
+})
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
