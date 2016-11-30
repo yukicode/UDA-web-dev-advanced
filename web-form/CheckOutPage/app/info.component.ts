486f907d10377fdb2354bdc9ab9a9c4fc26857fc
yukicode <yolandaapply@gmail.com>
2016-11-29 22:15:02 -0800

    add form validation

--- web-form/CheckOutPage/app/info.component.ts
+++ web-form/CheckOutPage/app/info.component.ts
@@ -1,13 +1,21 @@
-import {Component } from '@angular/core';
-import {Info} from './info';
+import { Component } from '@angular/core';
+import { Info } from './info';
 
-@Component ({
+@Component({
     moduleId: module.id,
     selector: 'my-info',
     templateUrl: './info.component.html',
-    styleUrls:['./info.component.css'],
+    styleUrls: ['./info.component.css'],
 })
 
-export class InfoComponent{
+export class InfoComponent {
     myInfo = new Info();
+    submitForm = function () {
+        this.myInfo.getIssues();
+    };
+    checkNumber = function () {
+        if (this.myInfo.cardNumber) {
+            this.myInfo.cardNumber = this.myInfo.cardNumber.replace(/[^0-9]/g, '');
+        }
+    };
 }
--- web-form/CheckOutPage/app/info.ts
+++ web-form/CheckOutPage/app/info.ts
@@ -6,13 +6,17 @@ export class Info {
     private _billingFirstName: string;
     private _billingLastName: string;
     private _billingAddress: string;
+
+    private _isSame = true;
     
+    isValid = true;
+    issues;
+
     cardNumber: string;
     zipCode: string;
     expMonth: number;
     expYear: number;
     cvv: number;
-    isSame = true;
 
     get firstName(): string {
         return this._firstName;
@@ -47,37 +51,81 @@ export class Info {
         }
     }
 
-    get billingFirstName(): string {
-        return this._billingFirstName;
+    get isSame(): boolean{
+        return this._isSame;
     }
 
-    set billingFirstName(name: string) {
-        this._billingFirstName = name;
-        if (this.isSame) {
-            this._firstName = name;
+    set isSame(s : boolean) {
+        this._isSame = s;
+        if(!this._isSame){
+            this._billingFirstName = this._firstName;
+            this._billingLastName = this._lastName;
+            this._billingAddress = this._address;
+        }else{
+            this._firstName = this._billingFirstName;
+            this._lastName = this._billingLastName;
+            this._address = this._billingAddress;
         }
     }
 
-    get billingLastName(): string {
-        return this._billingLastName;
+    getIssues(){
+        this.checkValidity();
+        if(this.issues){
+            this.isValid = false;
+        }else{
+            this.isValid = true;
+        }
+        return this.issues;
     }
 
-    set billingLastName(name: string) {
-        this._billingLastName = name;
-        if (this.isSame) {
-            this._lastName = name;
+    checkValidity(){
+        this.issues = "";
+        if(!this._firstName){
+            this.issues += "First name can't be empty \n";
+        }
+
+        if(!this._lastName){
+            this.issues += "Last name can't be empty \n";
+        }
+
+        if(!this._address){
+            this.issues += "Address can't be empty \n";
+        }
+
+        if(!this._billingFirstName){
+            this.issues += "Billing first name can't be empty \n";
+        }
+
+        if(!this._billingLastName){
+            this.issues += "Billing last name can't be empty \n";
+        }
+
+        if(!this._billingAddress){
+            this.issues += "Billing address can't be empty \n";
+        }
+
+        if(!this.cardNumber){
+            this.issues += "Card number can't be empty \n";
+        }else if(this.cardNumber.length !=16){
+            this.issues += "Incorrect length of credit card number \n";
+        }
+
+        if(!this.cvv){
+            this.issues += "Need cvv number \n";
+        }else if(this.cvv.toString().length != 3 ){
+            this.issues += "CVV length is not 3 \n";
+        }
+
+        if(!this.expMonth){
+            this.issues += "Need expire month \n";
+        }else if(this.expMonth < 1 || this.expMonth > 12){
+            this.issues += "Expire month is invalid \n";
+        }
+
+        if(!this.expYear){
+            this.issues += "Need expire year \n";
+        }else if(this.expYear < 2016){
+            this.issues += "Expire year is invalid \n";
         }
     }
-
-    get billingAddress(): string {
-        return this._billingAddress;
-    }
-
-    set billingAddress(address: string) {
-        this._billingAddress = address;
-        if (this.isSame) {
-            this._address = address;
-        }
-    }
-
 }
